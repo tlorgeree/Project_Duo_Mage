ab1d46de73f29669a0f0877acde06a4ee730b291
tlorgeree <71152544+tlorgeree@users.noreply.github.com>
2021-10-04 20:04:51 -0400

    Updated house sprite

--- Duo Mage Prototype/Assets/Scripts/Player_In_Building.cs
+++ Duo Mage Prototype/Assets/Scripts/Player_In_Building.cs
@@ -8,12 +8,13 @@ public class Player_In_Building : MonoBehaviour
     //and if in contact with it, makes the fac eof the building transparent.
     void OnTriggerEnter(Collider other)
     {
-        
         //Check for a match with the specified name on any GameObject that collides with your GameObject
         if (other.gameObject.tag == "House")
         {
-            GameObject child = other.transform.parent.gameObject.transform.GetChild(1).gameObject;//get sibling which is face
-          if (child.GetComponentInChildren<Renderer>().enabled == true) child.GetComponentInChildren<Renderer>().enabled = false; //Make building face invisible
+            GameObject child_door = other.transform.parent.gameObject.transform.GetChild(1).gameObject;//get sibling which is door
+            if (child_door.GetComponentInChildren<Renderer>().enabled == true) child_door.GetComponentInChildren<Renderer>().enabled = false; // make door invisible
+            GameObject child_face = other.transform.parent.gameObject.transform.GetChild(2).gameObject;//get sibling which is face
+            if (child_face.GetComponentInChildren<Renderer>().enabled == true) child_face.GetComponentInChildren<Renderer>().enabled = false; //Make building face invisible
           
         }
 
@@ -24,8 +25,10 @@ public class Player_In_Building : MonoBehaviour
     {
          if (other.gameObject.tag == "House")
             {
-                GameObject child = other.transform.parent.gameObject.transform.GetChild(1).gameObject;//Get sibling which is face
-                if (child.GetComponentInChildren<Renderer>().enabled == false) child.GetComponentInChildren<Renderer>().enabled = true;//Make building face visible
+                GameObject child_door = other.transform.parent.gameObject.transform.GetChild(1).gameObject;//Get sibling which is Door
+                if (child_door.GetComponentInChildren<Renderer>().enabled == false) child_door.GetComponentInChildren<Renderer>().enabled = true;//Make building door visible
+                GameObject child_face = other.transform.parent.gameObject.transform.GetChild(2).gameObject;//Get sibling which is face
+                if (child_face.GetComponentInChildren<Renderer>().enabled == false) child_face.GetComponentInChildren<Renderer>().enabled = true;//Make building face visible
 
             }
     }
